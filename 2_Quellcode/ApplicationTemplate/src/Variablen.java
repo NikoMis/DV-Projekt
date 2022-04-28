7a2a4e8f6204489effe924726886ca11eab9675e
marmacel <101929601+marmacel@users.noreply.github.com>
2022-04-28 18:34:37 +0200

    Add files via upload
    Tastenbelegung(Pfeiltasten)
    Variablen und die zugehörige Geschwindigkeit der Bewegung bei Ausführung (kann angepasst werden)

---
+++ 2_Quellcode/ApplicationTemplate/src/Variablen.java
@@ -0,0 +1,9 @@
+
+
+public class Variablen {
+	
+	static int speedup = 3, speeddown =2, speedleft=2, speedright=2;
+	static boolean moveup = false, movedown = false, moveleft = false, moveright = false;
+	
+	
+}
