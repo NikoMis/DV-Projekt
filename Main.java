f0d91868dc512e4e47d3d31921789ff9e3788c64
marmacel <101929601+marmacel@users.noreply.github.com>
2022-06-01 20:39:42 +0200

    Add files via upload
    Steuerbares Auto
    detect collision möglich
    Abbruch der Timerschleife bei collision + Anzeige "Game over Loser"
    //Florian //Marcel

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		new GUI_Racer();
+		new Variablen();
+		new Keys();
+		new Label();
+		new Movement();
+		new hintergrund();
+		
+	}
+}
---
+++ Movement.java
@@ -0,0 +1,41 @@
+ import java.util.Timer;
+import java.util.TimerTask;
+
+public class Movement {
+Timer Movement;
+
+  public  Movement() {
+	Movement = new Timer();
+	Movement.scheduleAtFixedRate(new TimerTask() {
+
+		@Override
+		public void run() {
+			if(Variablen.gameover == false) {
+			if(Variablen.moveup == true) {
+				if(Variablen.yAuto>0) {
+					Variablen.yAuto -= Variablen.speedup;
+					Variablen.Player.y -= Variablen.speedup;
+				}
+			}
+			else if(Variablen.movedown == true) {
+				if(Variablen.yAuto<=450) {
+					Variablen.yAuto += Variablen.speeddown;
+					Variablen.Player.y += Variablen.speeddown;
+				}
+			}
+				if(Variablen.moveleft == true) {
+					if(Variablen.xAuto>0) {
+						Variablen.xAuto -= Variablen.speedleft;
+						Variablen.Player.x -= Variablen.speedleft;
+					}
+				}
+				else if(Variablen.moveright == true) {
+					if(Variablen.xAuto<=750) {
+						Variablen.xAuto += Variablen.speedright;
+						Variablen.Player.x += Variablen.speedright;
+					}
+		}
+		}
+		
+		}},0,10);
+	}}
