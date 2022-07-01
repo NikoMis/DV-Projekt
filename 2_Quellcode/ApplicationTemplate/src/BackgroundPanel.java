1b746cd7a8c570e7abb9816b83042eefab99b11e
NikoMis <101878234+NikoMis@users.noreply.github.com>
2022-07-01 11:43:17 +0200

    Update BackgroundPanel.java
    //Nikolas

--- 2_Quellcode/ApplicationTemplate/src/BackgroundPanel.java
+++ 2_Quellcode/ApplicationTemplate/src/BackgroundPanel.java
@@ -4,19 +4,39 @@ import java.awt.Graphics;
 
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
+/**
+ * 
+ * @author Nikolas
+ * 
+ * @version 1.0
+ *
+ */
 
 public class BackgroundPanel extends JPanel {
-ImageIcon icon;
+	
+	private static final long serialVersionUID = 1L;
 
+ImageIcon icon;
+/**
+ * Initialisiert das Bild
+ * @param icon
+ */
+	
 public BackgroundPanel(ImageIcon icon) {
 	this.icon = icon;
 }
+/**
+ * Lädt das Bild rein und zeichnet es
+ */
 protected void paintComponent(Graphics f) {
 	super.paintComponent(f);
 	if (this.icon != null) {
 		f.drawImage(icon.getImage(), 0, 0, this);
 	}
 }
+/**
+ * Schneidet das Bild zurecht
+ */
 public Dimension getPreferredSize() {
 	if (icon != null) {
 		return new Dimension(icon.getIconWidth(),icon.getIconHeight());
