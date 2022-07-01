23ff3d2a85cf23442b0742735a92fcb39c14a8ba
NikoMis <101878234+NikoMis@users.noreply.github.com>
2022-07-01 11:40:58 +0200

    Update GUI_Game.java
    //Nikolas

--- 2_Quellcode/ApplicationTemplate/src/GUI_Game.java
+++ 2_Quellcode/ApplicationTemplate/src/GUI_Game.java
@@ -1,74 +1,61 @@
 import java.awt.Color;
-import java.awt.Container;
 import java.awt.EventQueue;
-import java.awt.FlowLayout;
 import java.awt.Font;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.WindowConstants;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JList;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar; 
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-import java.awt.Component;
 
-
+/**
+ * 
+ * @author Nikolas
+ * 
+ * @version 1.1
+ *
+ */
 public class GUI_Game extends JFrame{
 	
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
-	private int highscore = -1; //checkt ob im Programm überhaupt ein Highscore grad ist.
+	
 	public GUI_Game() {
 		
-		
-		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		//Bild laden
-		
-		/*add(new BackgroundPanel(new ImageIcon("C:\\Users\\nikol\\OneDrive\\Desktop\\tufftuff.jpg")));
+		add(new BackgroundPanel(new ImageIcon("C:\\Users\\nikol\\OneDrive\\Desktop\\auto.png")));
 		pack();
 		setVisible(true);
-		*/
-				
-		JPanel panel = new JPanel();
-		
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setTitle("High Speed Racer 2D");
 		//x = 700 , y = 600
 		setSize(700,600);
 		setLocationRelativeTo(null);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
-		
-		
+		JPanel panel = new JPanel();
 		
 		add(panel);
 		panel.setLayout(null);
-		panel.setBackground(Color.gray);
+		// Hintergrundfarbe für ein GUI ohne Bild
 		
+		//panel.setBackground(Color.red);
 		
+		// Erzeugt eine Überschrift.
+		 
 		
-		
-		
-		//Überschrift
 		Font font = new Font("Berlin Sans", Font.BOLD | Font.ITALIC, 20);
 		JLabel ueberschrift = new JLabel("High Speed Racer 2D");
 		ueberschrift.setFont(font);
 		ueberschrift.setBounds(250, 30, 205, 30);
-		ueberschrift.setBackground(Color.yellow);
+		ueberschrift.setBackground(Color.black);
 		ueberschrift.setOpaque(true);
+		setVisible(true);
 		panel.add(ueberschrift);
-		//Liste mit Highscores
-		JList<String> list = new JList<>();
-		//Image
 		
-		//Menubar
+		// Erzeugt das Menuebar
 		JMenuBar menu = new JMenuBar();
 		JMenu datei = new JMenu("Menü");
 		JMenuItem CG = new JMenuItem("Close Game");
@@ -80,30 +67,16 @@ public class GUI_Game extends JFrame{
 		menu.add(datei);
 		setJMenuBar(menu);
 		
-		
-		
-		//CloseButton mit 80 Länge und 30 Höhe als Standard
-		JButton button = new JButton("Close");
-		button.setBounds(595,500,80,30);
-		panel.add(button);
+		// Buttons mit 80 Länge und 30 Höhe als Standard
+		 
 		//PlayButton
 		JButton button1 = new JButton("Play");
 		button1.setBounds(300,250,80,30);
 		panel.add(button1);
 		
-		
-		//JButton button2 = new JButton("Close");
-		//JButton button3 = new JButton("Close");
-		//JButton button1 = new JButton("Play");
-	
-		button.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				System.exit(0);
-			}
-		});
+		//PlayButtonAction
 		button1.addActionListener(new ActionListener() {
 			
-
 			public void actionPerformed(ActionEvent e) {
 				setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 				Main.main(null);
@@ -111,37 +84,19 @@ public class GUI_Game extends JFrame{
 			}
 		});
 		
-		//Layout
-				//Container pane = getContentPane();
-				//pane.setLayout(new FlowLayout(FlowLayout.CENTER));
-				//pane.add(button1);
-				//pane.add(button);
-				//pane.add(button1);
-		
-				
-		
 	}
 	
-
-	/*private void bildLaden() {
-		FileDialog d = new FileDialog(this, "Bilddatei laden...", FileDialog.LOAD);
-		d.setVisible(true);
-		m_hfulogo = d.getDirectory() + d.getFile();
-		
-	}*/
-
-
 	public static void main(String[] args) {
+		
 		new GUI_Game();
 		
 		
 		EventQueue.invokeLater(new Runnable()
 				{
-			public void run() {
+		public void run() {
 				GUI_Game g = new GUI_Game();
 				g.setVisible(true);
 			}
 				});
 		}
 }
-
